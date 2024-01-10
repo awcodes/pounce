cb6f5ee15a4ed6b12442d80935cf521489d31e88
Adam Weston <awcodes1@gmail.com>
2024-01-10 16:33:52 -0500

    add gap to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
 
 <div
     {{ $attributes->class([
-        'fi-modal-footer p-4 flex items-center',
+        'fi-modal-footer p-4 flex items-center gap-4',
         match($align) {
             'start' => 'justify-start',
             'center' => 'justify-center',
